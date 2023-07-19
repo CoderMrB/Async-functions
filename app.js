f44da0b46e30b082906e085b93cf1c6d9e7bc515
Chris Branson <chrisbranson@live.co.uk>
2023-07-19 21:01:02 +0100

    Added a further async function and random number generator to insert meme on random quote generation

--- app.js
+++ app.js
@@ -4,8 +4,12 @@ let quoteGenerator = document.getElementById("new-quote-button")
 let quotesList= document.getElementById("quote-history")
 quoteGenerator.addEventListener('click', getQuote)
 let randomQuote = ''
+let memeImage = document.getElementsByTagName('img')[0]
+let memetext=document.createElement('h4')
 
 async function getQuote(){
+    memeImage.setAttribute('src', 'https://static.vecteezy.com/system/resources/thumbnails/000/381/000/small_2x/Education_2__28200_29.jpg')
+    memetext.innerHTML=''
     let newLi = document.createElement("li")
     let currentList = document.querySelectorAll("li")
     let repeatQuote = false
@@ -31,8 +35,26 @@ async function getQuote(){
 
     let quote = await fetch(`http://type.fit/api/quotes`);
     let result = await quote.json();
-    randomQuote = result[Math.floor(Math.random()*3)].text;
-    h1.textContent = randomQuote    
+    randomQuote = result[Math.floor(Math.random()*1643)].text;
+    h1.textContent = randomQuote
+
+    
+    
+
+    let randomNumber = Math.floor(Math.random()*10)
+
+     if (randomNumber === 9){
+        let memedata = await fetch("https://api.imgflip.com/get_memes")
+        let memeresult = await memedata.json()
+        console.log(memeresult)
+        let meme = memeresult.data.memes[Math.floor(Math.random()*99)].url
+        memeImage.setAttribute('src', meme)
+        memetext.textContent="You earned a random meme!"
+        memeImage.after(memetext)
+        let docbody = document.getElementsByTagName('body')[0]
+        docbody.prepend(memeImage)
+     }
+
 }
 
 

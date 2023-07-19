fa6b1bef24abe6994ff0a9df355757d45c2cc108
Chris Branson <chrisbranson@live.co.uk>
2023-07-19 19:44:07 +0100

    Async function created which returns a random quote as JSON

---
+++ app.js
@@ -0,0 +1,9 @@
+console.log("hello world")
+
+async function getQuote(){
+    let quote = await fetch(`http://type.fit/api/quotes`);
+    let result = await quote.json();
+    console.log(result[Math.floor(Math.random()*1643)].text);
+}
+
+getQuote()

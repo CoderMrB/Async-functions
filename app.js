88fa3215830b9707b11682684cc5acf8032e11e4
Chris Branson <chrisbranson@live.co.uk>
2023-07-19 19:52:52 +0100

    Used DOM methods to set H1 text content to randomly selected quote

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 console.log("hello world")
+let h1 = document.getElementById("quote")
 
 async function getQuote(){
     let quote = await fetch(`http://type.fit/api/quotes`);
     let result = await quote.json();
-    console.log(result[Math.floor(Math.random()*1643)].text);
+    let randomquote = result[Math.floor(Math.random()*1643)].text;
+    h1.textContent = randomquote
 }
 
-
 getQuote()
+
+

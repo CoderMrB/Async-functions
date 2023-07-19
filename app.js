811c3865549cc49c348b26ad0c4caa08bb192bf9
Chris Branson <chrisbranson@live.co.uk>
2023-07-19 20:13:42 +0100

    Added conditional logic to add a list of the past generated quotes to the page

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
 console.log("hello world")
 let h1 = document.getElementById("quote")
 let quoteGenerator = document.getElementById("new-quote-button")
+let quotesList= document.getElementById("quote-history")
 quoteGenerator.addEventListener('click', getQuote)
+let randomQuote = ''
 
 async function getQuote(){
+    let newLi = document.createElement("li")
+    if (randomQuote !== ''){
+        newLi.textContent = randomQuote
+        quotesList.appendChild(newLi)
+        }
     let quote = await fetch(`http://type.fit/api/quotes`);
     let result = await quote.json();
-    let randomquote = result[Math.floor(Math.random()*1643)].text;
-    h1.textContent = randomquote
+    randomQuote = result[Math.floor(Math.random()*1643)].text;
+    h1.textContent = randomQuote
+    
+    
 }
-
-getQuote()
 
 

549b96adc9cb4a3f8fb53eb1f15ad4c3111147e7
Chris Branson <chrisbranson@live.co.uk>
2023-07-19 20:34:03 +0100

    Used conditional logic to loop through nodelist of list items and prevent repeat quotes being added to list

--- app.js
+++ app.js
@@ -7,16 +7,32 @@ let randomQuote = ''
 
 async function getQuote(){
     let newLi = document.createElement("li")
+    let currentList = document.querySelectorAll("li")
+    let repeatQuote = false
+   
+
     if (randomQuote !== ''){
+        for(i=0; i<currentList.length;i++){
+            if (randomQuote === currentList[i].textContent){
+                repeatQuote = true;
+                break
+            }else{
+                continue
+            }
+        }
+    }
+   
+    if (repeatQuote === false && randomQuote !== ''){
         newLi.textContent = randomQuote
         quotesList.appendChild(newLi)
         }
+        
+
+
     let quote = await fetch(`http://type.fit/api/quotes`);
     let result = await quote.json();
-    randomQuote = result[Math.floor(Math.random()*1643)].text;
-    h1.textContent = randomQuote
-    
-    
+    randomQuote = result[Math.floor(Math.random()*3)].text;
+    h1.textContent = randomQuote    
 }
 
 
